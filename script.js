b32359d25992439ef183c478db08661792fec0db
ogzA <akankan.oguz@gmail.com>
2026-07-21 21:22:31 +0200

    fix: move scripts accordingly to script.js file

--- script.js
+++ script.js
@@ -13,3 +13,40 @@ function renderBooks() {
 		renderBooksRef.innerHTML += getBookTemplate(book, booksIndex);
 	}
 }
+
+function getLikeStatus(liked) {
+	if (!liked) {
+		return /*html*/ `
+            <img class="heart" src="./assets/icons/heart.png" width="50" alt="">`;
+	} else {
+		return /*html*/ `
+            <img class="heart" src="./assets/icons/heart_full.png" width="50" alt="">    
+        `;
+	}
+}
+
+function updateLikeStatus(booksIndex) {
+	const book = books[booksIndex];
+	book.liked = !book.liked;
+
+	if (!book.liked) {
+		book.likes--;
+	} else {
+		book.likes++;
+	}
+
+	renderBooks();
+}
+
+function addComment(booksIndex) {
+	const commentInput = document.getElementById(`comment-input-${booksIndex}`);
+	const commentInputValue = commentInput.value;
+
+	if (commentInputValue !== "") {
+		books[booksIndex].comments.push({
+			name: "Guest",
+			comment: commentInputValue,
+		});
+	}
+	renderBooks();
+}

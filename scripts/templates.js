9bee99fb0c8bd988a53fd16daf26bcb741af5501
ogzA <akankan.oguz@gmail.com>
2026-07-22 11:56:08 +0200

    fix: comment section: input & send button

--- scripts/templates.js
+++ scripts/templates.js
@@ -41,7 +41,9 @@ function getBookTemplate(book, booksIndex) {
                 </div>
                 <div class="add-comment">
                     <input id="comment-input-${booksIndex}" placeholder="Kommentar hinzufügen" type="textarea">
-                    <button onclick="addComment(${booksIndex})">Hinzufügen</button>
+                    <button class="add-comment__button" onclick="addComment(${booksIndex})">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-send-icon lucide-send"><path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z"/><path d="m21.854 2.147-10.94 10.939"/></svg>
+                    </button>
                 </div>
             </section>
         </article>

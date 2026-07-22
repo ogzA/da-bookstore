66c976ba6bd52945df774216571d7afa361b8555
ogzA <akankan.oguz@gmail.com>
2026-07-22 14:56:35 +0200

    fix: fixes

--- scripts/templates.js
+++ scripts/templates.js
@@ -4,7 +4,6 @@ function getBookTemplate(book, booksIndex) {
             <header> <h2>${book.name}</h2></header>
             <section class="section-img" id="section-img">
                 <img src="./assets/img/book-element.png" height="75" alt="">
-            </button>
             </section>
             <section class="section-details" id="section-details">
                 <div class="price-and-like">
@@ -38,7 +37,7 @@ function getBookTemplate(book, booksIndex) {
                     ${getCommentsTemplate(book.comments)}
                 </div>
                 <div class="add-comment">
-                    <input id="comment-input-${booksIndex}" placeholder="Kommentar hinzufügen" type="textarea">
+                    <input id="comment-input-${booksIndex}" placeholder="Kommentar hinzufügen" type="text">
                     <button class="add-comment__button" onclick="addComment(${booksIndex})">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-send-icon lucide-send"><path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z"/><path d="m21.854 2.147-10.94 10.939"/></svg>
                     </button>
